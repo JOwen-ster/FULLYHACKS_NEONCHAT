648b46a5520381397684e0279d1bc29e367a0845
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2024-02-25 02:15:32 -0800

    update before trying

--- gui.py
+++ gui.py
@@ -1,14 +1,17 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import scrolledtext
+import server
+import client
 
 root = tk.Tk()
 
 
-root.geometry('500x500')
-root.title('NeonTooth')
 
-label = tk.Label(root, text="Test", font=('Arial', 24))
+root.geometry('500x500')
+root.title('Neon Chat')
+
+label = tk.Label(root, text="NEON CHAT", font=('Arial', 24))
 label.pack(padx=20, pady=20)
 
 def enter(event):
@@ -19,7 +22,7 @@ def enter(event):
         if event.state == 9:
            msg.delete(tk.END) 
         elif event.state == 8:
-            hello()
+            get_text()
 
 def stopText(event):
     if event:
@@ -33,15 +36,19 @@ textbox.pack(padx=10, pady=10)
 textbox.bind("<KeyPress>", enter)
 
 
-def hello():
+def get_text():
     new = textbox.get('1.0', tk.END).strip()
+    if not new:
+        return
     msg.insert(tk.INSERT, new + "\n")
     textbox.delete(1.0, tk.END)
+    client.send(new.encode("utf-8"))
 
 photo = tk.PhotoImage(file = r"images/neonbutton100x34.png")
-tk.Button(root, image = photo, command=hello).pack(side=tk.TOP)
+tk.Button(root, image = photo, command=get_text).pack(side=tk.TOP)
 
 
-
+tk.Button(root, text="host", command=server.run_server).pack(side=tk.TOP)
+tk.Button(root, text="join", command=client.run_client).pack(side=tk.TOP)
 
 root.mainloop()
