32b3e211bb3c77228b4e21b20f9417a2e50e9524
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2024-02-25 16:39:15 -0800

    fixed merge conflict
    W

--- gui.py
+++ gui.py
@@ -57,34 +57,4 @@ tk.Button(root, image = imgHost, command=runServer).pack(side=tk.LEFT, padx=30)
 imgJoin = tk.PhotoImage(file = r"images/neonbuttonjoin200x68.png")
 tk.Button(root, image = imgJoin, command=runClient).pack(side=tk.RIGHT, padx=30)
 
-# def enter(event):
-#     # prints to check key
-#     # print(event.state)
-#     # print(event.keysym)
-#     if event.keysym == "Return":
-#         if event.state == 9:
-#            msg.delete(tk.END) 
-#         elif event.state == 8:
-#             get_text()
-
-# def stopText(event):
-#     if event:
-#         msg.delete(tk.END)
-
-# msg = scrolledtext.ScrolledText(root, height=6, font=('Arial', 12))
-# msg.bind("<KeyPress>", stopText)
-# msg.pack(padx=10, pady=10)
-# textbox = tk.Text(root, height=1, font=('Arial', 12))
-# textbox.pack(padx=10, pady=10)
-# textbox.bind("<KeyPress>", enter)
-
-
-# def get_text():
-#     new = textbox.get('1.0', tk.END).strip()
-#     if not new:
-#         return
-#     msg.insert(tk.INSERT, new + "\n")
-#     textbox.delete(1.0, tk.END)
-#     client.send(new.encode("utf-8"))
-
 root.mainloop()
